4b87402f565eaee79eee8c8cedbefba3d67d7ab1
yayab <yannis.bounia@outlook.com>
2025-08-16 20:32:50 +0200

    Update : Updated URL paths for a better SEO compatibility.

--- ouaf/ouaf/urls.py
+++ ouaf/ouaf/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('ouaf_app.urls')),
+    path("index/", include('ouaf_app.urls')),
+    path("", RedirectView.as_view(url="/index/", permanent=True)),
 ]
